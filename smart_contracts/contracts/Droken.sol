6fd88a0660a9b2b5eb8030fae0b8e99b2aa56fe9
Óscar Ortiz <oscortiz@gmail.com>
2020-02-12 10:30:30 +0100

    Cambios menores en contrato Droken y tests
    Se formatea el código en ambos y se añaden comentarios para documentar funciones en el contrato Droken.sol.

--- smart_contracts/contracts/Droken.sol
+++ smart_contracts/contracts/Droken.sol
@@ -4,12 +4,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
+/** @title Droken.*/
 contract Droken is ERC20, ERC20Detailed, Ownable {
-
-    constructor(uint256 initialSupply) ERC20Detailed("Droken", "DRK", 18) public {
+    /**@dev Crea un nuevo token con los siguientes parámetros:
+     * - Nombre: Droken
+     * - Símbolo: DRK
+     * - Número de decimales utilizados para obtener su representación: 18
+     * @param initialSupply Cantidad de tokens (Droken) a crear.
+     */
+    constructor(uint256 initialSupply)
+        public
+        ERC20Detailed("Droken", "DRK", 18)
+    {
         _mint(msg.sender, initialSupply);
     }
 
+    /**@dev Crea nuevos tokens (Droken) y los asigna a la cuenta incrementando la cantidad total.
+     * @param amount Cantidad de tokens (Droken) a crear.
+     */
     function mint(uint256 amount) public onlyOwner {
         _mint(msg.sender, amount);
     }
--- smart_contracts/test/Droken.test.js
+++ smart_contracts/test/Droken.test.js
@@ -1,55 +1,108 @@
-const Droken = artifacts.require('Droken');
+const Droken = artifacts.require("Droken");
+const truffleAssert = require("truffle-assertions");
 
-contract('Droken Tests', accounts => {
-
+contract("Droken Tests", accounts => {
     let droken;
 
-    beforeEach(async() => {
+    beforeEach(async () => {
         droken = await Droken.new(5000, { from: accounts[0] });
     });
 
-    it('Contrato se inicia correctamente', async () => {
+    it("Contrato se inicia correctamente", async () => {
         const name = await droken.name.call();
         const symbol = await droken.symbol.call();
         const decimals = await droken.decimals.call();
         const totalSupply = await droken.totalSupply.call();
-        
-        assert.equal(name, 'Droken', 'El nombre del Droken no se inicia correctamente');
-        assert.equal(symbol, 'DRK', 'El símbolo del Droken no se inicia correctamente');
-        assert.equal(decimals, 18, 'El número de decimales del Droken no se inicia correctamente');
-        assert.equal(totalSupply, 5000, 'El número de Drokens creado no se inicia correctamente');
+
+        assert.equal(
+            name,
+            "Droken",
+            "El nombre del Droken no se inicia correctamente"
+        );
+        assert.equal(
+            symbol,
+            "DRK",
+            "El símbolo del Droken no se inicia correctamente"
+        );
+        assert.equal(
+            decimals,
+            18,
+            "El número de decimales del Droken no se inicia correctamente"
+        );
+        assert.equal(
+            totalSupply,
+            5000,
+            "El número de Drokens creado no se inicia correctamente"
+        );
     });
 
-    it('Añadidos nuevos Drokens', async () => {
+    it("Añadidos nuevos Drokens", async () => {
         const tokensIniciales = await droken.totalSupply.call();
-        await droken.mint(1000, { from: accounts[0] });
+        const tx = await droken.mint(1000, { from: accounts[0] });
         const tokensFinales = await droken.totalSupply.call();
-        
-        assert.equal(Number(tokensFinales), Number(tokensIniciales) + 1000, 'El número total de Drokens existentes no es correcto');
-    });    
 
-    it('Solo el owner puede añadir nuevos Drokens', async () => {
+        // emit Transfer(sender, recipient, amount);
+        truffleAssert.eventEmitted(tx, "Transfer", ev => {
+            return (
+                ev.from == '0x0000000000000000000000000000000000000000' &&
+                ev.to == accounts[0] &&
+                ev.value == 1000
+            );
+        });  
+        assert.equal(
+            Number(tokensFinales),
+            Number(tokensIniciales) + 1000,
+            "El número total de Drokens existentes no es correcto"
+        );
+    });
+
+    it("Solo el owner puede añadir nuevos Drokens", async () => {
         try {
             await droken.mint(1000, { from: accounts[1] });
-        } catch(err) {
+        } catch (err) {
             // Funcionamiento correcto: No deja minar Drokens
-            error = err; 
-            assert.isAbove(error.message.search('VM Exception while processing transaction: revert'), -1, 'Ownable: caller is not the owner');
-		}
-    });  
-    
-    it('Drokens transferidos correctamente', async () => {
+            error = err;
+            assert.isAbove(
+                error.message.search(
+                    "VM Exception while processing transaction: revert"
+                ),
+                -1,
+                "Ownable: caller is not the owner"
+            );
+        }
+    });
+
+    it("Drokens transferidos correctamente", async () => {
         const tokensIniciales = await droken.totalSupply.call();
         const saldoInicialFrom = await droken.balanceOf(accounts[0]);
         const saldoInicialTo = await droken.balanceOf(accounts[1]);
-        await droken.transfer(accounts[1], 1000);
+        const tx = await droken.transfer(accounts[1], 1000);
         const saldoFinalFrom = await droken.balanceOf(accounts[0]);
         const saldoFinalTo = await droken.balanceOf(accounts[1]);
         const tokensFinales = await droken.totalSupply.call();
-        
-        assert.equal(Number(tokensFinales), Number(tokensIniciales), 'El número total de Drokens existentes no es correcto');
-        assert.equal(Number(saldoFinalFrom), Number(saldoInicialFrom) - 1000, 'El saldo final del ordenante no es correcto');
-        assert.equal(Number(saldoFinalTo), Number(saldoInicialTo) + 1000, 'El saldo final del destinatario no es correcto');
-    }); 
 
+        // emit Transfer(sender, recipient, amount);
+        truffleAssert.eventEmitted(tx, "Transfer", ev => {
+            return (
+                ev.from == accounts[0] &&
+                ev.to == accounts[1] &&
+                ev.value == 1000
+            );
+        });        
+        assert.equal(
+            Number(tokensFinales),
+            Number(tokensIniciales),
+            "El número total de Drokens existentes no es correcto"
+        );
+        assert.equal(
+            Number(saldoFinalFrom),
+            Number(saldoInicialFrom) - 1000,
+            "El saldo final del ordenante no es correcto"
+        );
+        assert.equal(
+            Number(saldoFinalTo),
+            Number(saldoInicialTo) + 1000,
+            "El saldo final del destinatario no es correcto"
+        );
+    });
 });
