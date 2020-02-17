a0976381cdcf34035660998a0a9f5a07b1104e4c
Iker Prego <ikerprego@gmail.com>
2020-02-17 18:40:00 +0100

    Bocetar funciones contratarDron y asignarDron de Dronchain

--- smart_contracts/contracts/DronChain.sol
+++ smart_contracts/contracts/DronChain.sol
@@ -13,6 +13,8 @@ contract DronChain is Ownable {
     ParcelasERC721 parcelasContract;
     Empresas empresasContract;
 
+    mapping(uint256 => mapping(uint256 => bool)) private dronesContratados;
+
     event DronContratado(uint256 dronId, uint256 parcelaId);
     event ParcelaFumigada(uint256 parcelaId, uint256 dronId);
 
@@ -100,15 +102,38 @@ contract DronChain is Ownable {
         empresasContract.registrarEmpresa(msg.sender, nombre, cif);
     }
 */
-    /*
+
     function contratarDron(uint256 dronId, uint256 parcelaId) public {
-        //TODO
+        require(!dronesContratados[dronId][parcelaId], 'El dron seleccionado ya había sido contratado para fumigar dicha parcela');
+        (, address empresaParcela, , , ) = parcelasContract.getParcela(parcelaId);
+        require(empresaParcela == msg.sender, 'El usuario no es el dueño de la parcela');
+        (, address empresaDron, , , , uint256 coste) = dronesContract.getDron(dronId);
+
+        // registramos la contratacion del dron para fumigar esa parcela
+        dronesContratados[dronId][parcelaId] = true;
+
+        // el dueño de la parcela autoriza a la empresa del dron a gastar una cantidad de droken igual al coste del dron
+        drokenContract.approve(empresaDron, coste);
+
+        // emitimos el evento DronContratado(uint256 dronId, uint256 parcelaId)
+        emit DronContratado(dronId,parcelaId);
     }
 
     function asignarDron(uint256 dronId, uint256 parcelaId) public {
-        //TODO
+        require(dronesContratados[dronId][parcelaId], 'El dron seleccionado no había sido contratado para fumigar dicha parcela');
+
+        // actualizamos la contratacion del dron para fumigar dicha parcela
+        dronesContratados[dronId][parcelaId] = false;
+
+        // hacemos la transferencia de droken del dueño de la parcela a la empresa que gestiona el dron
+        (, address empresaParcela, , , ) = parcelasContract.getParcela(parcelaId);
+        (, address empresaDron, , , , uint256 coste) = dronesContract.getDron(dronId);
+        drokenContract.transferFrom(empresaParcela, empresaDron, coste);
+
+        // emitimos el evento ParcelaFumigada(uint256 parcelaId, uint256 dronId)
+        emit ParcelaFumigada(parcelaId, dronId);
     }
-*/
+
 
     /**
      * @dev Se lanza si es llamado por cualquier cuenta de empresa que no exista
