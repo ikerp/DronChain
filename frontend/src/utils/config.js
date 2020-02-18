8f9e1693b77f21b32ef4fa0124f8aa1a772e5795
Óscar Ortiz <oscortiz@gmail.com>
2020-02-18 21:04:46 +0100

    Añadir fichero de configuraciones previas (utilidades)

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,7 @@
+// Identificadores del tipo de usuario
+export const PROPIETARIO = 'propietario';
+export const EMPRESA = 'empresa';
+export const ANONIMO = 'anonimo';
+export const SIN_METAMASK = 'sin metamask';
+
+// Lista de pesticidas disponibles
