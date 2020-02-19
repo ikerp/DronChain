497dfed6fee0e27efa33acfd6d704e6dd33cd7c6
Óscar Ortiz <xoo1001@alu.ubu.es>
2020-02-19 17:44:33 +0100

    Añadir lista de pesticidas a configuración inicial

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -5,3 +5,10 @@ export const ANONIMO = 'anonimo';
 export const SIN_METAMASK = 'sin metamask';
 
 // Lista de pesticidas disponibles
+export const PESTICIDAS = {
+    '1' : 'Pesticida A',
+    '2' : 'Pesticida B',
+    '3' : 'Pesticida C',
+    '4' : 'Pesticida D',
+    '5' : 'Pesticida E'
+}
