c13f86ec6557782e64ed7d46debe5db551f8c9ab
Óscar Ortiz <xoo1001@alu.ubu.es>
2020-02-26 17:52:18 +0100

    Utilidad para obtener contratos pendientes por dron

---
+++ frontend/src/utils/contratosPendientes.js
@@ -0,0 +1,41 @@
+const contratosPendientes = (dronChain, dronId) => 
+    new Promise((resolve, reject) => {
+
+        let contratosPendientes = [];
+    
+        try {
+            dronChain.getPastEvents('DronContratado', {
+                fromBlock:0,
+                toBlock:'latest'            
+            })
+            .then(eventos => {
+                contratosPendientes = eventos.filter(evento => 
+                    Number(evento.returnValues.dronId) === Number(dronId)
+                ).map(ev => 
+                    ev.returnValues.parcelaId
+                );
+        
+                dronChain.getPastEvents('ParcelaFumigada', {
+                    fromBlock:0,
+                    toBlock:'latest'            
+                })
+                .then(eventos => {
+                    eventos.filter(evento => 
+                        Number(evento.returnValues.dronId) === Number(dronId)
+                    ).map(ev => {
+                        let index = contratosPendientes.indexOf(ev.returnValues.parcelaId);
+                        if (index !== -1) {
+                            contratosPendientes.splice(index, 1);
+                        }
+                    });   
+
+                    resolve(contratosPendientes);
+                })  
+            })
+        } catch (error) {
+            console.error('ERROR en Promise contratosPendientes');
+            reject(); 
+        }
+    });
+
+export default contratosPendientes;
