b2ecf9369120567c64e662e2ab820ae924c9f9e0
Óscar Ortiz <xoo1001@alu.ubu.es>
2020-02-28 13:19:06 +0100

    Componente indicador de valores cargando (spinner)

---
+++ frontend/src/componentes/Spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Spinner(props) {
+    return (
+        <div className="d-flex justify-content-center pt-5">
+            <div className="sk-chase">
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
