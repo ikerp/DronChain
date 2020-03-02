e587abafb64b17d5a990bb3630ce8463a9037808
Óscar Ortiz <xoo1001@alu.ubu.es>
2020-03-02 17:44:21 +0100

    Modificado truffle-config.js
    Añadida red de desarrollo para Alastria

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -11,6 +11,15 @@ module.exports = {
       port: 7545,
       network_id: '*', // matchear cualquier red contra la que se lancen los scripts
       // gas: 15000000 // limite de gas por transaccion
+    },
+    // Red de desarrollo de Alastria
+    testnetAlastria: {
+      host: '192.168.56.101',
+      port: 22001,
+      network_id: '*', // matchear cualquier red contra la que se lancen los scripts
+      gas: 0xfffff, // limite de gas por transaccion
+      gasPrice: 0x0,
+      from: '0x74d4c56d8dcbc10a567341bfac6da0a8f04dc41d'
     }
   },
   compilers: {
